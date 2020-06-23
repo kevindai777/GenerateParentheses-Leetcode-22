3eda2ce040f5c2d8c15c9c046e7491e758759be9
kevindai777 <66235906+kevindai777@users.noreply.github.com>
2020-06-23 02:37:31 -0400

    edits to solution and explanation

--- generateparentheses.js
+++ generateparentheses.js
@@ -4,24 +4,27 @@
 let n = 3
 
 
-//O() solution
+//O(4^n/sqrt(n)) solution
 
 let result = []
 backtrack('', n, n)
 
 function backtrack(temp, left, right) {
+    //Once all parentheses are used up...
     if (left == 0 && right == 0) {
         result.push(temp)
         return
     }
 
+    //Use up one set of parentheses
     if (left > 0) {
         backtrack(temp + '(', left - 1, right)
     }
 
+    //Then use up the other set
     if (right > left) {
         backtrack(temp + ')', left, right - 1)
     }
 }
 
-console.log(result)
+return result
